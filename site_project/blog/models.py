14bc0a83a4aaaa222316757e789a7398ab4d8d18
Murad Maili <muradmailimurad@gmail.com>
2022-02-23 15:52:50 +0400

    changes

--- site_project/blog/models.py
+++ site_project/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.forms import CharField
 
-# Create your models here.
+class Course(models.Model):
+    name=models.CharField(max_length=200)
+    description=models.TextField(blank=True,null=True)
+    image=models.ImageField(upload_to="courses/%Y/%m/%d/")
+    date=models.DateTimeField(auto_now=True)
+    avaliable=models.BooleanField(default=True)
